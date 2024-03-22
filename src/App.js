7d6483d3ea6907072e1ec77331937e76e653e382
dev <devsatish08@gmail.com>
2024-03-22 23:33:13 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'https://book-finder1.p.rapidapi.com/api/search';
+const API_KEY = '589737d0c2mshd5724a7cf3ed992p12857bjsn20e22a13855b';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [favourites, setfavourites] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [page, setPage] = useState(1);
+  const [reading, setReading] = useState(false);
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+  const [buttonText, setButtonText] = useState('Dark Mode');
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+
+  const toggleColors = () => {
+    setIsDarkMode(!isDarkMode);
+    !isDarkMode ? setButtonText('Dark Mode') : setButtonText('Light Mode');
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, [page, author, title]);
+
+  const readText = (quote) => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = quote;
+
+    if (!reading) {
+      window.speechSynthesis.speak(msg);
+      setReading(true);
+    } else {
+      window.speechSynthesis.cancel();
+      setReading(false);
+    }
+  };
+
+  const handleNameSearch = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const handleAuthorSearch = (event) => {
+    setAuthor(event.target.value);
+  }
+
+  const fetchBooks = async () => {
+    let url = `${API_URL}?lexile_min=600&lexile_max=800&results_per_page=24&page=${page}`;
+
+    if (author.trim() !== '') {
+      url += `&author=${author}`;
+    }
+
+    if (title.trim() !== '') {
+      url += `&title=${title}`;
+    }
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': 'book-finder1.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log('Fetched data:', data);
+      setBooks(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const togglefavourite = (book) => {
+    if (favourites.some(fav => fav.work_id === book.work_id)) {
+      setfavourites(favourites.filter(fav => fav.work_id !== book.work_id));
+    } else {
+      setfavourites([...favourites, book]);
+    }
+  };
+
+  const showfavourites = () => {
+    setBooks(favourites);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainBody" style={{ backgroundColor: isDarkMode ? 'black' : 'white' }}>
+      <div className='header' style={{ color: !isDarkMode ? 'white' : 'black' }}>
+        <div className="titleName">bookie</div>
+        <button className="button1" style={{ color: !isDarkMode ? 'white' : 'black' }} onClick={toggleColors}>{buttonText}</button>
+        <button className="button1" style={{ color: !isDarkMode ? 'white' : 'black' }} onClick={showfavourites}>Show favourites</button>
+        <div className="menuBarMain">
+          <input className="menuBar" type="search" style={{ color: !isDarkMode ? 'white' : 'black' }} onChange={handleNameSearch} value={title} placeholder="Search by Name" />
+          <input className="menuBar" type="search" style={{ color: !isDarkMode ? 'white' : 'black' }} onChange={handleAuthorSearch} value={author} placeholder="Search by Author" />
+        </div>
+      </div>
+
+      <div className="bookCard" style={{ color: !isDarkMode ? 'white' : 'black' }}>
+        {books && books.length > 0 ? (
+          books
+            .filter(book => book.published_works[0].isbn)
+            .map(book => (
+              <div key={book.work_id} className="bookBox">
+                <img
+                  className="coverImg"
+                  src={`https://s3.amazonaws.com/mm-static-media/books/cover-art/${book.published_works[0].isbn}.jpeg`}
+                  alt={book.title}
+                />
+                <h2 className="bookTitle">{book.title}</h2>
+                <p>Author: {book.authors[0]}</p>
+                <p>Published: {book.published_works[0].copyright}</p>
+                {book.categories && book.categories.length >= 2 && (
+                  <p className="summary">{book.categories[1]}</p>
+                )}
+                <p>Genre : {book.book_type}</p>
+                <button className="readButton" onClick={() => readText(book.summary ? book.summary : "Summary not Found")}>
+                  Read Summary
+                </button>
+                <button className="favouriteBtn" onClick={() => togglefavourite(book)}>
+                  {favourites.some(fav => fav.work_id === book.work_id) ? "Remove from favourites" : "Add to favourites"}
+                </button>
+              </div>
+            ))
+        ) : (
+          <p>Loading...</p>
+        )}
+
+      </div>
+      <div className="footer">
+        <button className="button2" type="button" style={{ color: !isDarkMode ? 'white' : 'black' }} onClick={handlePreviousPage} disabled={page === 1}>PREVIOUS</button>
+        <button className="button2" type="button" style={{ color: !isDarkMode ? 'white' : 'black' }} onClick={handleNextPage}>NEXT</button>
+      </div>
     </div>
   );
 }
